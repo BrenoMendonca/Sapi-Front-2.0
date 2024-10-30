296875280925411197b39c3b71e90357937d1f91
unknown <breno_a99@hotmail.com>
2024-10-30 16:40:28 -0300

    Subindo a rota de requisitos

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { PaginaEdital } from './Pages/PaginaEdital/PaginaEdital';
 import { PaginaSubmissao } from './Pages/PaginaSubmissao/PaginaSubmissao';
 import { PaginaProfessores } from './Pages/PaginaProfessores/PaginaProfessores';
 import { MinhaConta } from './Pages/MinhaConta/MinhaConta';
+import {Login2} from './Pages/Login2/Login2'
 
   function App() {
   return (
@@ -19,6 +20,7 @@ import { MinhaConta } from './Pages/MinhaConta/MinhaConta';
         <Route path="/edital/:id" element={<PaginaEdital />} />
         <Route path="/edital/:idEdital/submissao/:idSubmissao" element={<PaginaSubmissao />} />
       </Route>
+      <Route path="/login2"element= {<Login2/>}/>
       <Route path="/login"element= {<Login/>}/>
       <Route path="/"element= {<Login/>} />
 
