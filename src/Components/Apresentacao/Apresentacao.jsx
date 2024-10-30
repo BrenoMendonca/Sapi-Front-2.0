3ed2d3f9b75a089d14d726186cdebc30191d6d71
unknown <breno_a99@hotmail.com>
2024-10-30 16:41:34 -0300

    Atualizando o componente de apresentação

--- src/Components/Apresentacao/Apresentacao.jsx
+++ src/Components/Apresentacao/Apresentacao.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import { useEffect , useState } from 'react';
 import '../Apresentacao/Apresentacao.css'
 
 export const Apresentacao = () =>{
+
+    const [name, setName] = useState(null);
+
+    useEffect(() => {
+        const sessionData = localStorage.getItem('session');
+        if (sessionData) {
+          const session = JSON.parse(sessionData);
+          setName(session.name);
+        }
+      }, []);
+
+
     return(
         <div className='background-apresentacao'>
             <div className='wrapper-apresentacao'>
-                <h1 className='apresentacao'>Sistema de projetos de Inovação</h1>
+                <h1 className='apresentacao'>Olá, {name} 👋</h1>
             </div>
         </div>
     )
