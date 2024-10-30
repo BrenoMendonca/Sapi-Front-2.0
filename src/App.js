e81ae611dfaecd6817b7312ed618b186321bf91a
unknown <breno_a99@hotmail.com>
2024-10-30 16:53:58 -0300

    Corrigindo erro na rota

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { PaginaEdital } from './Pages/PaginaEdital/PaginaEdital';
 import { PaginaSubmissao } from './Pages/PaginaSubmissao/PaginaSubmissao';
 import { PaginaProfessores } from './Pages/PaginaProfessores/PaginaProfessores';
 import { MinhaConta } from './Pages/MinhaConta/MinhaConta';
-import {Login2} from './Pages/Login2/Login2'
+import Requisitos from './Pages/Requisitos/Requisitos';
 
   function App() {
   return (
@@ -19,8 +19,8 @@ import {Login2} from './Pages/Login2/Login2'
         <Route path ="/minhaconta" element = {<MinhaConta/>}/>
         <Route path="/edital/:id" element={<PaginaEdital />} />
         <Route path="/edital/:idEdital/submissao/:idSubmissao" element={<PaginaSubmissao />} />
+        <Route path="/requisitos-padrao" element={<Requisitos />} />
       </Route>
-      <Route path="/login2"element= {<Login2/>}/>
       <Route path="/login"element= {<Login/>}/>
       <Route path="/"element= {<Login/>} />
 
