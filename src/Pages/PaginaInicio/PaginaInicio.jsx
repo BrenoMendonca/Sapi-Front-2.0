1b2c85a04da155e44476aa979f9fc8b7757addac
unknown <breno_a99@hotmail.com>
2024-10-30 16:43:12 -0300

    Atualizando a página de inicio

--- src/Pages/PaginaInicio/PaginaInicio.jsx
+++ src/Pages/PaginaInicio/PaginaInicio.jsx
@@ -1,46 +1,43 @@
-
-import './../PaginaInicio/paginaInicio.css';
+import '../PaginaInicio/paginaInicio.css';
 import Navbar from '../../Components/Navbar/Navbar';
-import UserService from '../../Services/Services';
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
-import {ModalCadastroEdital} from '../../Components/ModalCadastroEdital/ModalCadastroEdital';
-import { Load } from '../../Components/Load/Load'
+import { Load } from '../../Components/Load/Load';
 import Apresentacao from '../../Components/Apresentacao/Apresentacao';
-// Importando imagens
-import vizualizar from '../../Assets/olho.png';
-import editar from '../../Assets/escrever.png';
-import { Status } from '../../Components/Status/Status';
+import { ModalProfessores } from '../../Components/ModalProfessores/ModalProfessores';
 
-//import { Load } from '../../Components/Load/Load';
+import {ModalCadastroEdital} from '../../Components/ModalCadastroEdital/ModalCadastroEdital'
+import { VisaoGrid } from '../../Components/VisaoGrid/VisaoGrid'; // Importando o VisaoGrid
+import { VisaoCard } from '../../Components/VisaoCard/VisaoCard'; // Importando o VisaoCard
 
-export const  PaginaInicio = ()=>{
-    const [typeOfUser, setTypeOfUser] = useState(null)
+export const PaginaInicio = () => {
+    const [typeOfUser, setTypeOfUser] = useState(null);
 
-    //Modal
+    // Modal
     const [modalIsOpen, setIsOpen] = useState(false);
-    //Crud Editais
-    const [editais, setEditais] = useState([])
-    const [load, setLoad] = useState(false)
+    // Crud Editais
+    const [editais, setEditais] = useState([]);
+    const [load, setLoad] = useState(false);
+    // Visão CRUD, agora com 'grid' como padrão
+    const [visao, setVisao] = useState('grid'); 
 
-    // CAPTURAR DADOS DO PARAMETRO DA URL
-    //const {state} = useLocation()
-    //const [codigo, setCodigo] = useState(state.codigo)
-  
-    const getEditais = async () =>{
-        setLoad(true)
-        try{
-            const response = await axios.get("http://localhost:3001/getEdital/")
+    // Utilização dos filtros - Professores
+    const [filteredEditais, setFilteredEditais] = useState([]); 
+    const [searchTerm, setSearchTerm] = useState(''); 
+    const [selectedStatus, setSelectedStatus] = useState('');
 
+    const getEditais = async () => {
+        setLoad(true);
+        try {
+            const response = await axios.get("http://localhost:3001/getEdital/");
             const data = response.data;
-
+            console.log(response.data);
             setEditais(data);
-
-        } catch(error){
-            console.error(error)
+            console.log(data);
+        } catch (error) {
+            console.error(error);
         } finally {
-            setLoad(false)
+            setLoad(false);
         }
     }
 
@@ -62,103 +59,93 @@ export const  PaginaInicio = ()=>{
         }   
     }
 
+    const handleFiltroChange = (event) => {
+        setSearchTerm(event.target.value); 
+    };
 
-    useEffect(()=>{
-        getEditais()
-        getTypeOfUser()
-    },[])
+    const handleChange = (event) => {
+        setVisao(event.target.value);
+    };
+
+    useEffect(() => {
+        const normalize = (str) => (str ? str.toLowerCase() : '');
+        
+        // Filtragem dos editais com base nos critérios fornecidos
+        const filtered = editais.filter((edital) => {
+            const nomeMatch = normalize(edital.nameEdital).includes(normalize(searchTerm));
+            const numeroMatch = normalize(edital.numeroEdital).includes(normalize(searchTerm));
+            const statusMatch = selectedStatus ? normalize(edital.status) === normalize(selectedStatus) : true;
+    
+            // Retorna apenas editais que correspondem ao searchTerm e ao status selecionado
+            return (nomeMatch || numeroMatch) && statusMatch;
+        });
+    
+        setFilteredEditais(filtered);
+    }, [searchTerm, selectedStatus, editais]);
+
+    useEffect(() => {
+        getEditais();
+        getTypeOfUser();
+    }, []);
 
     const handleAtualizarListaEditais = async () => {
-        await getEditais(); // Aguarda um breve atraso antes de atualizar os editais
+        await getEditais();
     }
 
-    const buscarEdital = async () =>{
-        
-        //buscar editais
-        // tratar caso nao tenha ou ter dado erro
-        // if(sem editais){
-        // setLoad(false)
-        // setEditais(null)
-        // setAlerta('sem editais')
-        // return 
-        //}
-        // setar os editais em um estado
-        // setEditais(editaisJson)
-        // setLoad(false)
-    }
-    
-    return(
-            <div className='PaginaInicio'>
-                {/* <Sidebar></Sidebar> */}
-                <Navbar></Navbar>
-                <Apresentacao></Apresentacao>
-            {modalIsOpen &&(
+    return (
+        <div className='PaginaInicio'>
+            <Navbar />
+            {modalIsOpen && (
                 <ModalCadastroEdital setView={setIsOpen} atualizarListaEditais={handleAtualizarListaEditais} />
-            )} 
-        
+            )}
+    
             <div className='BackgroundPaginaInicio'>
-                <div className="container">
-                <div className="header">
-                    <h1>Editais</h1>
-                    
-                    {typeOfUser > 2 && ( 
-                        <button className='button-criaredital' onClick={() => setIsOpen(true)}>Criar Edital</button>
-                    )}
-                    
+                <Apresentacao />
+    
+                <div className='div-table-header'>
+                    <div className='header-apresentacao'>
+                        <h2 className='apresentacao-filtros'>Editais</h2>
+                        <div className='visao-editais'>
+                            <select className='visao-opcoes' onChange={handleChange} value={visao}>
+                                <option value="" disabled>Selecione o tipo de visão</option>
+                                <option value="grid">Grid</option>
+                                <option value="card">Card</option>
+                            </select>
+                        </div>
+                    </div>
+    
+                    <div className='header-filtros-edital'>
+                        <div className="filtro-pesquisa-edital">
+                            <input 
+                                className='filtro-pesquisa-edital-input' 
+                                type="text" 
+                                placeholder="Pesquisar..." 
+                                value={searchTerm} 
+                                onChange={handleFiltroChange} 
+                            />
+                        </div>
+                        {/*
+                        <div className="filtro-pesquisa-curso">
+                            <select className='filtro-pesquisa-curso-select'>
+                                <option value="" disabled selected>Selecione o curso</option>
+                            </select>
+                        </div>
+                        */}
+                        <div className="filtro-select-status">
+                            <select className='filtro-selecao-status-select'>
+                                <option value="" disabled selected>Selecione o status</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
-            
-                <div className="divTable">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th className='titulo-crud'>Num Edital</th>
-                            <th className='titulo-crud'>Objetivo</th>
-                            <th className='titulo-crud'>Titulo do Edital</th>
-                            <th className='titulo-crud centralizar-elemento'>Prazo para envio</th>
-                            <th className="titulo-crud centralizar-elemento">Status</th>
-                            <th className="titulo-crud centralizar-elemento">Ações</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                    
-                        {editais != null &&(
-                            editais.slice().reverse().map((edital) => 
-                            <tr style={{maxHeight: '5rem'}} key={edital._id}>
-                                <td className='num-edital'>
-                                    <Link to={`/edital/${edital._id}`}>
-                                        {edital.numeroEdital}
-                                    </Link>
-                                </td>
-                                <td className='limit-size objetivo'>{edital.objetivo}</td>
-                                <td className='limit-size'>{edital.nameEdital}</td>
-                                <td className='centralizar-elemento'>{edital.dataFinal}</td>
-                                <td className='centralizar-elemento'><Status status={edital.status}/></td>
-                                <td className='centralizar-elemento'>
-                                    <Link to={`/edital/${edital._id}`}>
-                                        <img alt="" title="Ver mais informações" className= 'visualizar'src={vizualizar}></img>
-                                    </Link>
-
-                                    <img alt="" title="Editar" className='editar' src= {editar}></img>
-                                </td>
-                            </tr>
-                        ))}
-
-                        <tr>
-                            <td colSpan="6" className="loading-cell">
-                                <div className="loading-container">
-                                    {load && <Load />}
-                                </div>
-                            </td>
-                        </tr>
-                        
-                        </tbody>
-                    </table>
+    
+                <div className="container-grid">
+                    {visao === 'grid' && <VisaoGrid editais={editais} />}
+                    {visao === 'card' && <VisaoCard editais={editais} />}
                 </div>
             </div>
         </div>
-    </div>
-        )
-}
+    );
+};
 
 export default PaginaInicio;
